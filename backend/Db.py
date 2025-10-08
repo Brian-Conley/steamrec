517dec20cf292bd8081e76925cccea4e9d6c4202
Brian-Conley <brianjconleyjr@gmail.com>
2025-10-08 16:13:13 -0400

    Add a method to query a game by appid

--- backend/Db.py
+++ backend/Db.py
@@ -5,3 +5,7 @@ class Db:
     def __init__(self, filename):
         self.db = sqlite3.connect(filename)
         self.cur = self.db.cursor()
+
+    def query_game_by_appid(self, appid):
+        game = self.cur.execute(f"SELECT * FROM Games WHERE appid={appid}")
+        return game.fetchone()
