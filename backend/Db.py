cb5c8ffd00e01c6a626f3584dc5ab95919308698
Brian-Conley <brianjconleyjr@gmail.com>
2025-10-15 19:07:46 -0400

    Add a query for all of a game's categories using appid

--- backend/Db.py
+++ backend/Db.py
@@ -9,27 +9,36 @@ class Db:
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
             game = cur.execute(
-                    "SELECT * FROM Games WHERE appid = ?",
+                    "SELECT * FROM games WHERE appid = ?",
                     (appid,))
-            return game.fetchall()
+            return game.fetchone()
 
     def query_game_by_name(self, name):
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
-            game = cur.execute("SELECT * FROM Games WHERE name LIKE ?",
+            game = cur.execute("SELECT * FROM games WHERE name LIKE ?",
                                (f"%{name}%",))
             return game.fetchall()
+
+    def query_categories_by_appid(self, appid):
+        with sqlite3.connect(self.filename) as conn:
+            cur = conn.cursor()
+            categories = cur.execute("""
+                                     SELECT * FROM game_categories
+                                     WHERE appid = ?
+                                     """, (appid,))
+            return categories.fetchall()
 
     def query_category_by_id(self, cid):
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
-            cat = cur.execute("SELECT * FROM Categories WHERE id = ?",
+            cat = cur.execute("SELECT * FROM categories WHERE id = ?",
                               (cid,))
             return cat.fetchall()
 
     def query_category_by_name(self, name):
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
-            cat = cur.execute("SELECT * FROM Categories WHERE name LIKE ?",
+            cat = cur.execute("SELECT * FROM categories WHERE name LIKE ?",
                               (f"%{name}%",))
             return cat.fetchall()
