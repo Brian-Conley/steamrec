5e6d2d2cac8e1c583ca025f046bdc764ee55dfe9
Brian-Conley <brianjconleyjr@gmail.com>
2025-10-17 12:51:10 -0400

    Fix some errors

--- backend/Db.py
+++ backend/Db.py
@@ -6,6 +6,15 @@ class Db:
         self.filename = filename
 
     def query_game_by_appid(self, appid):
+        """
+        Fetch a game row by its appid
+
+        Params:
+            appid (int): Steam appid
+
+        Returns:
+            tuple: All values related to the game
+        """
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
             game = cur.execute(
@@ -14,6 +23,15 @@ class Db:
             return game.fetchone()
 
     def query_game_by_name(self, name):
+        """
+        Fetch a game row by its name
+
+        Params:
+            name (string): The game's name (partial match supported)
+
+        Returns:
+            tuple: All values related to the game
+        """
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
             game = cur.execute("SELECT * FROM games WHERE name LIKE ?",
@@ -21,35 +39,67 @@ class Db:
             return game.fetchall()
 
     def query_categories_by_appid(self, appid):
+        """
+        Fetch all categories associated with an appid
+
+        Params:
+            appid (int): The game's appid
+
+        Returns:
+            list: All category ids associated with the appid
+        """
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
             categories = cur.execute("""
-                                     SELECT * FROM game_categories
+                                     SELECT categoryid FROM game_categories
                                      WHERE appid = ?
                                      """, (appid,))
-            return categories.fetchall()
+            cids = categories.fetchall()
+            return [c[0] for c in cids]
 
     def query_category_by_id(self, cid):
+        """
+        Fetch a category name by its id
+
+        Params:
+            cid (int): The category's id
+
+        Returns:
+            string: Category name
+        """
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
-            cat = cur.execute("SELECT * FROM categories WHERE id = ?",
+            cat = cur.execute("SELECT name FROM categories WHERE id = ?",
                               (cid,))
-            return cat.fetchall()
+            return cat.fetchone()[0]
 
     def query_category_by_name(self, name):
+        """
+        Fetch a category id by its name
+
+        Params:
+            name (string): The category's name
+
+        Returns:
+            id (int): The category's id
+        """
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
-            cat = cur.execute("SELECT * FROM categories WHERE name LIKE ?",
-                              (f"%{name}%",))
-            return cat.fetchall()
+            cat = cur.execute("""
+                              SELECT id FROM categories
+                              WHERE name LIKE ?
+                              """, (f"%{name}%",))
+            return cat.fetchone()[0]
 
     def query_game_count(self):
+        # Total number of games
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
             count = cur.execute("SELECT COUNT(*) FROM games")
             return count.fetchone()[0]
 
     def query_category_count(self):
+        # Total number of categories
         with sqlite3.connect(self.filename) as conn:
             cur = conn.cursor()
             count = cur.execute("SELECT COUNT(*) FROM categories")
