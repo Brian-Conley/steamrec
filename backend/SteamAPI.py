310ea5f213b38359e249b3f7169ea363f47c3da5
Brian-Conley <brianjconleyjr@gmail.com>
2025-10-24 00:48:54 -0400

    Add a method to fetch a user's owned games + playtime

--- backend/SteamAPI.py
+++ backend/SteamAPI.py
@@ -36,3 +36,41 @@ class SteamAPI:
             # Request failed
             return None
         return response.json()
+
+    def GetOwnedGames(self, steamid):
+        """
+        Get a list of appids of games the user owns + free games they've played.
+        https://developer.valvesoftware.com/wiki/Steam_Web_API#GetOwnedGames_(v0001)
+
+        Params:
+            steamid: number or string 'id'
+
+        Returns:
+            None if request fails
+            number of games (int), dict of games{appid: total playtime}
+                playtime is in minutes
+        """
+
+        url = self.url_base + "IPlayerService/GetOwnedGames/v0001/"
+        params = {
+                "key": self.api_key,
+                "steamid": steamid,
+                "include_played_free_games": True
+        }
+
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            # Request failed
+            print(response.status_code)
+            return None
+
+        response = response.json().get('response')
+        game_count = int(response.get('game_count'))
+        games = response.get('games', [])
+        games_filtered = {}
+        for game in games:
+            appid = int(game.get('appid'))
+            total_playtime = int(game.get('playtime_forever'))
+            games_filtered[appid] = total_playtime
+
+        return game_count, games_filtered
