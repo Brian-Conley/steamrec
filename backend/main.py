22bc1ed173d9d9d97df8274b9f84f53daed4fba6
Brian-Conley <brianjconleyjr@gmail.com>
2025-11-10 14:15:16 -0500

    Update database and compress it

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,10 @@ import routes.db.game
 import routes.db.insert
 import routes.db.delete
 import routes.db.update
+import tarfile
 
 
 if __name__ == "__main__":
+    with tarfile.open("steam_games.db.tar.gz", "r:gz") as tar:
+        tar.extractall(path=".")
     app.app.run(host="0.0.0.0", port=5000, debug=True)
